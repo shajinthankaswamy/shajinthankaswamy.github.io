0df0d184f921c7a9e856d940c4d430443f7d42e4
Shajin Thankaswamy <shajinthankaswamy@gmail.com>
2026-07-15 00:40:54 +0200

    Remove ctrl k text from search button and clean theme entries from search palette

---
+++ assets/js/search-cleanup.js
@@ -0,0 +1,19 @@
+(function() {
+  function removeThemeEntries() {
+    const ninja = document.querySelector('ninja-keys');
+    if (ninja && ninja.data) {
+      ninja.data = ninja.data.filter(function(item) {
+        return item.section !== 'Theme';
+      });
+    }
+  }
+  if (document.readyState === 'complete') {
+    removeThemeEntries();
+  } else {
+    document.addEventListener('readystatechange', function() {
+      if (document.readyState === 'complete') {
+        removeThemeEntries();
+      }
+    });
+  }
+})();
